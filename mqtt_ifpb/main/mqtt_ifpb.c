af11839a09eab5b01a0632698bc96870470738ab
Pedro Augusto Gonçalves Lucena <pedro30052005mat@gmail.com>
2026-04-02 15:13:50 -0300

    adicionando atividade mqtt

---
+++ mqtt_ifpb/main/mqtt_ifpb.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "mqtt_client.h"
+#include "esp_log.h"
+
+static const char *TAG = "MQTT_TUTORIAL";
+
+/* TROQUE PELO IP DO SEU MAC */
+#define BROKER_URL "mqtt://10.22.230.115"
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    esp_mqtt_event_handle_t event = event_data;
+
+    switch ((esp_mqtt_event_id_t)event_id) {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "Conectado ao Broker!");
+            esp_mqtt_client_publish(event->client, "ifpb/c6/status", "ESP32 Ativo", 0, 1, 0);
+            break;
+
+        case MQTT_EVENT_DISCONNECTED:
+            ESP_LOGW(TAG, "MQTT Desconectado");
+            break;
+
+        default:
+            break;
+    }
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    ESP_ERROR_CHECK(example_connect());
+
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = BROKER_URL,
+    };
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
